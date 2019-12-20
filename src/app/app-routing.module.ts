f07fdaa124f37a40d2e715bfc1a4cfb6e688b608
shipingzhong@gmail.com <958186957@qq.com>
2019-12-20 19:39:54 +0800

    2012-12-20 19:39

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PostComponent } from './post/post.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:PostComponent
+  },
+  {
+    path:'users',
+    loadChildren:()=>import('./user/user.module').then(module=>module.UserModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
